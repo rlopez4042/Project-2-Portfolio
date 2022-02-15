9cc699da23849518c2e7d2a6a7f742d811036655
Robert Lopez <robertlopez@Roberts-MacBook-Pro.local>
2022-02-15 11:59:14 -0800

    new block of code for fetching through an array

--- surfsd/src/Components/Home.js
+++ surfsd/src/Components/Home.js
@@ -2,28 +2,31 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 // import axios from "axios";
 import { useParams } from "react-router-dom";
+import spotIDs from "../spotID-data";
 
 function Home(props) {
   //A variable for the spot ID, each beach has a unique spot ID
-  const [beach,setBeach]=useState([
-    {name:"Pacific Beach",spotID:"5842041f4e65fad6a7708841"},
-    {name:"Ocean Beach",spotID:"5842041f4e65fad6a770883f"},
-    {name:"Imperial Beach",spotID:"5842041f4e65fad6a7708847"},
-])
-  const { spot } = useParams();
+  //const { spot } = useParams();
+  //Base URL, loaded with IB data to isolate bugs
+  //const beachFinder = `https://services.surfline.com/kbyg/spots/reports?spotId=5842041f4e65fad6a7708847`;
   //Base URL
-  const beachFinder = `https://services.surfline.com/kbyg/spots/reports?spotId=5842041f4e65fad6a7708847`;
+  const beachFinder = `https://services.surfline.com/kbyg/spots/reports?spotId=`;
   const [beachData, setBeachData] = useState(null);
+  
   //Set initialBeach to null to avoid loading a URL without a spot ID on initial launch
   let initialBeach = null;
   //Gather data from the URL
-  const findBeach = () => {
-    fetch(beachFinder)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        setBeachData(data);
-      });
+  const findBeach = async () => {
+    var arrayLength = spotIDs.length;
+    for (var i = 0; i < arrayLength; i++) {
+      fetch(beachFinder + spotIDs[i])
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          setBeachData(data);
+        //initialBeach = data.spot.name;
+        });
+    }
   };
   useEffect(() => {
     findBeach();
---
+++ surfsd/src/spotID-data.js
@@ -0,0 +1,15 @@
+const spotIDs = [
+  //Pacific Beach
+  "5842041f4e65fad6a7708841",
+  //Ocean Beach
+  "5842041f4e65fad6a770883f",
+  //Imperial Beach
+  "5842041f4e65fad6a7708847",
+];
+export default spotIDs;
+
+// const [beach, setBeach] = useState([
+//   { name: "Pacific Beach", spotID: "5842041f4e65fad6a7708841" },
+//   { name: "Ocean Beach", spotID: "5842041f4e65fad6a770883f" },
+//   { name: "Imperial Beach", spotID: "5842041f4e65fad6a7708847" },
+// ]);
